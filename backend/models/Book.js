1632174453402ec54f3ce4483bb8025f91b4957a
saidugurlu <ugurlumsaid@gmx.de>
2022-07-07 11:40:47 +0200

    e-mail added in authour schema

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
-
 const authorSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    url: String
+	firstName: String,
+	lastName: String,
+	url: String,
+	email: String,
 });
-
+// olusturdugumuz her schema nin kendine ait otomatik bir id si olur.
 const bookSchema = new mongoose.Schema({
 	title: String,
 	description: String,
@@ -15,9 +15,9 @@ const bookSchema = new mongoose.Schema({
 	imageUrl: String,
 	buyUrl: String,
 	whenPurchased: Date,
-relatedBook: mongoose.SchemaTypes.ObjectId,
-topics: [String],
-author: authorSchema
+	relatedBook: mongoose.SchemaTypes.ObjectId,
+	topics: [String],
+	author: authorSchema,
 });
 
 export const Book = mongoose.model('book', bookSchema);
